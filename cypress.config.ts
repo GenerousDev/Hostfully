d1d49502440ad9db5f4f87644a6de8b623502f54
GenerousDev <omolaja.abubakar05@gmail.com>
2023-05-05 08:17:44 +0100

    Installed all the necessary dependency and updated the cypress.config.ts with the new setup

---
+++ cypress.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from "cypress";
+import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
+import browserify from "@badeball/cypress-cucumber-preprocessor/browserify";
+
+async function setupNodeEvents(
+  on: Cypress.PluginEvents,
+  config: Cypress.PluginConfigOptions
+): Promise<Cypress.PluginConfigOptions> {
+  
+  await addCucumberPreprocessorPlugin(on, config);
+
+  on(
+    "file:preprocessor",
+    browserify(config, {
+      typescript: require.resolve("typescript"),
+    })
+  );
+
+  
+  return config;
+}
+
+export default defineConfig({
+  e2e: {
+    viewportHeight: 900,
+    viewportWidth: 1400,
+    defaultCommandTimeout: 10000,
+    baseUrl: 'https://computer-database.gatling.io/computers',
+    specPattern: "**/*.feature",
+    setupNodeEvents,
+  },
+});
